a34334267e82746beec8d058ca84637a62167267
CTSI <ctsi@adinet.com.uy>
2023-01-03 10:39:26 -0300

    lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { LoginComponent } from "./components/login/login.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "login", pathMatch: "full"},
-  { path: "**", redirectTo: "login", pathMatch: "full"},
   { path: "login", component: LoginComponent},
   { path: "dashboard", loadChildren: () => import('./components/dashboard/dashboard.module').then( x => x.DashboardModule)},
+  { path: "**", redirectTo: "login", pathMatch: "full"},
 ]
 
 @NgModule({
